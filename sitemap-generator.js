7404489e6966e637547a1a50291847a19cd6793e
keisuke ohashi <kog71903@gmail.com>
2020-01-09 23:19:56 +0900

    first commit

---
+++ sitemap-generator.js
@@ -0,0 +1,16 @@
+const SitemapGenerator = require('sitemap-generator');
+ 
+// create generator
+const generator = SitemapGenerator('https://imitsu.jp/', {
+  stripQuerystring: false
+});
+ 
+// register event listeners
+generator.on('done', () => {
+  // sitemaps created
+});
+ 
+// start the crawler
+generator.start();
+
+
